8d23ab9968a29a5637ae665a40085299ccff6338
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-08-27 09:55:53 +0800

    update

---
+++ xcaddy_path.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+xcaddy_path=${xcaddy_path}
+
+echo "touch Dockerfile"
+
+touch Dockerfile
+
+echo "Supplementing Dockerfile with xcaddy path"
+
+cat >>Dockerfile <<EOF
+FROM golang:alpine AS builder
+
+RUN go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+
+WORKDIR /build
+
+RUN xcaddy build --with $xcaddy_path
+
+FROM alpine:latest
+
+RUN apk add --no-cache ca-certificates libcap mailcap
+
+COPY --from=builder /build/caddy /usr/bin/caddy
+
+RUN chmod 700 /usr/bin/caddy
+
+COPY ./Caddyfile /etc/caddy/Caddyfile
+
+EXPOSE 80 443 443/udp 2019
+
+CMD ["/usr/bin/caddy", "run", "--config", "/etc/caddy/Caddyfile", "--adapter", "caddyfile"]
+EOF
